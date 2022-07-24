dfe3f6082013c07e91ea191773a2093ef9c2e7e0
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-24 12:01:01 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,20 @@
+import { SignUpController } from "./signup";
+
+describe("SignUp Controller", () => {
+  test("Should return 400 if no name is provided", () => {
+    // sut = system under test
+    const sut = new SignUpController();
+    // requisição post que ele vai receber
+    const httpRequest = {
+      body: {
+        email: "matheus@teste.com",
+        password: "teste",
+        passwordConfirmation: "teste",
+      },
+    };
+    //espera que esse valor seja 400 pois não foi passado o 'name'
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
