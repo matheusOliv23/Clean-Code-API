68e5b3948759e5f08c47889c851c21e94c86cf82
matheusOliv23 <matheushenri1996@gmail.com>
2022-08-22 22:39:59 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from "./signup";
+import { MissingParamError } from "../errors/missing.param.error";
 
 describe("SignUp Controller", () => {
   test("Should return 400 if no name is provided", () => {
@@ -16,24 +17,22 @@ describe("SignUp Controller", () => {
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
-    test("Should return 400 if no email is provided", () => {
-      const sut = new SignUpController();
-      const httpRequest = {
-        body: {
-          name: "Matheus",
-          password: "teste",
-          passwordConfirmation: "teste",
-        },
-      };
+  test("Should return 400 if no email is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "Matheus",
+        password: "teste",
+        passwordConfirmation: "teste",
+      },
+    };
 
-      const httpResponse = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest);
 
-      expect(httpResponse.statusCode).toBe(400);
-      expect(httpResponse.body).toEqual(new Error("Missing param: email"));
-    });
-
-  
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
 });
